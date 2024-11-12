679f9805376a731768677b8c924ce5ba24485cb3
Eric Clemens <clemens72@gmail.com>
2024-11-12 15:33:16 -0500

    added /api to app/api/auth/...

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import React from 'react'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
